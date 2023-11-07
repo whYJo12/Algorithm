87717f8ab6bbe274c7a22b7af778402768a2b698
whYJo12 <147726971+whYJo12@users.noreply.github.com>
2023-11-07 16:46:20 +0900

    [Bronze V] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,10 @@
+def function():
+    A,B,C = map(int, input().split())
+
+    print((A+B)%C)
+    print(((A%C) + (B%C))%C)
+    print((A*B)%C)
+    print(((A%C)*(B%C))%C)
+
+if __name__ == "__main__":
+    function()
