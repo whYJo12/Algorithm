c2c0a0e3283479e113a353c1a25acacfa58a43ff
whYJo12 <147726971+whYJo12@users.noreply.github.com>
2023-11-07 16:46:43 +0900

    [Bronze V] Title: 사칙연산, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,11 @@
+def function():
+    A,B = map(int, input().split())
+
+    print(A + B)
+    print(A - B)
+    print(A * B)
+    print(A // B)
+    print(A % B)
+
+if __name__ == "__main__":
+    function()
