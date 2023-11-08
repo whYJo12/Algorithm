4cd67ea575d5e420104fe3971921a869bce342e4
whYJo12 <147726971+whYJo12@users.noreply.github.com>
2023-11-08 23:52:23 +0900

    [Silver V] Title: 사탕, Time: 232 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11256. 사탕/사탕.py
+++ 백준/Silver/11256. 사탕/사탕.py
@@ -1,16 +1,17 @@
 def function():
     T = int(input())  # 테스트 케이스 수
 
-    for _ in range(T):  # 테스트 케이스 수 만큼 반복        
+    for _ in range(T):  # 테스트 케이스 수 만큼 반복
         J, N = map(int, input().split())
         boxSize = []
+        count = 0
+        
         for i in range(N):
             R,C = map(int, input().split()) # 상자 가로세로 길이를 입력받아 저장
             boxSize.append(R * C) # 상자크기 (가로*세로)
 
         boxSize.sort(reverse=True) # 상자크기를 내림차순으로 정렬
 
-        count = 0
         while J > 0:
             J -= boxSize[count]  # 사탕을 박스에 넣었으니 사탕갯수를 박스사이즈만큼 빼기
             count += 1  # 상자가 1개 쓰였으니 카운트세기
