c6c488d12a104b6bb929600989138930e1444611
whYJo12 <147726971+whYJo12@users.noreply.github.com>
2023-11-10 01:03:08 +0900

    [Bronze V] Title: 새싹, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,10 @@
+def function():
+       print("         ,r'\"7")
+       print("r`-_   ,'  ,/")
+       print(" \. \". L_r'")
+       print("   `~\/")
+       print("      |")
+       print("      |")
+
+if __name__ == "__main__":
+    function()
