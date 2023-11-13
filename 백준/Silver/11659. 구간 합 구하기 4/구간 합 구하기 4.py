f5572838c0f897590d2408f3832dd411e925f98c
whYJo12 <147726971+whYJo12@users.noreply.github.com>
2023-11-13 23:29:34 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 212 ms, Memory: 41116 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,17 @@
+import sys
+
+def function():
+    N, M = map(int, input().split())
+    nList = list(map(int, sys.stdin.readline().strip().split()))
+    prefix_sum = [0] * (N + 1)
+
+    for i in range(N):
+        prefix_sum[i + 1] = prefix_sum[i] + nList[i]
+
+    for _ in range(M):
+        A, B = map(int, sys.stdin.readline().strip().split())
+        partSum = prefix_sum[B] - prefix_sum[A - 1]
+        print(partSum)
+
+if __name__ == "__main__":
+    function()
