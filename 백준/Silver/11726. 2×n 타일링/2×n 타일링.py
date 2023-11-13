6e88dfcb3197fbca99225b377c2d4b7b6b489a45
whYJo12 <147726971+whYJo12@users.noreply.github.com>
2023-11-13 19:46:09 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,16 @@
+import sys
+
+def function():
+
+    N = int(sys.stdin.readline())
+    dp = [0] * 1001
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, N + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+
+    print(dp[N])
+
+if __name__ == "__main__":
+    function()
