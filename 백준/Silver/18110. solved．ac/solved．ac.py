df52012df190a1945d225e0a5d62f3ff6ee3cca2
whYJo12 <147726971+whYJo12@users.noreply.github.com>
2023-11-22 23:41:55 +0900

    [Silver IV] Title: solved.ac, Time: 272 ms, Memory: 55088 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -0,0 +1,26 @@
+import sys
+import statistics
+import decimal
+import math
+
+EPS = 1e-9
+def my_round(val):
+    return int(val) + 1 if val - int(val) >= 0.5 else int(val)
+
+def function():
+    N = int(input())
+    if N != 0:
+        level = list(int(sys.stdin.readline()) for _ in range(N))
+        level.sort(reverse=True)
+
+        trimmedMean = my_round(N * 0.15)
+
+        apply_truc = sorted(level)[trimmedMean: N - trimmedMean]
+        mean = my_round(sum(apply_truc) / len(apply_truc))
+        
+        print(mean)
+    else:
+        print(0)
+
+if __name__ == "__main__":
+    function()
