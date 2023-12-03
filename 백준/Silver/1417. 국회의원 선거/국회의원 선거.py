aa9f1cd21723c5987bff9f21092f14fb946685a0
whYJo12 <147726971+whYJo12@users.noreply.github.com>
2023-12-03 18:53:09 +0900

    [Silver V] Title: 국회의원 선거, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,17 +1,22 @@
-n = int(input()) # 후보의 수
-dasom = int(input()) # 기호 1번을 찍으려고 하는 사람의 수
-vote = [] # 다른 후보를 찍으려고 하는 사람의 수
-count = 0 # 매수해야하는 사람의 수
-for _ in range(n - 1):
-  vote.append(int(input()))
-vote.sort(reverse=True) # 내림차순 정렬
-if n == 1:
-  print(0)
-else:
-  # 표가 제일 많은 후보를 찍으려고 하는사람을 매수
-  while vote[0] >= dasom:
-    dasom += 1
-    vote[0] -= 1
-    count += 1
-    vote.sort(reverse=True)
-  print(count)
+def function():
+    N = int(input())
+    dasom = int(input())
+    votesList = []
+    for _ in range(N - 1):
+        votesList.append(int(input()))
+    buy = 0
+
+    votesList.sort(reverse=True)
+
+    if N == 1:
+        print(0)
+    else:
+        while votesList[0] >= dasom:
+            dasom += 1
+            buy += 1
+            votesList[0] -= 1
+            votesList.sort(reverse=True)
+        print(buy)
+
+if __name__ == "__main__":
+    function()
